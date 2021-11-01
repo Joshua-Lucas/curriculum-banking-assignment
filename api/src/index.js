21bd4ab3fe28b1ce3f075540b0a519f5d01d47ab
Joshua Lucas <joshuamlucas91@gmail.com>
2021-11-01 13:40:19 -0400

    update the message returned

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello to the Bank API for Introduction to React!')
 })
 
 app.listen(port, () => {
