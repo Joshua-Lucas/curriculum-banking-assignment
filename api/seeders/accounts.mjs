1718a686fbc96759014d6b092e5ff17fef25c5e1
Joshua Lucas <joshuamlucas91@gmail.com>
2021-12-08 18:03:07 -0500

    change account creation date to be before trans actions and create account types in db

--- api/seeders/accounts.mjs
+++ api/seeders/accounts.mjs
@@ -1,15 +1,19 @@
-import { createAccount } from "../src/models/accountsModel.mjs";
+import faker from "faker";
+import { createSeedAccounts } from "../src/models/accountsModel.mjs";
 
 const accounts = [
   {
+    created_date: "2021-09-08 22:40:36",
     account_type: "checking",
     customer_id: 1,
   },
   {
+    created_date: "2021-11-05 22:40:36",
     account_type: "checking",
     customer_id: 2,
   },
   {
+    created_date: "2021-11-28 22:40:36",
     account_type: "saving",
     customer_id: 2,
   },
@@ -17,6 +21,6 @@ const accounts = [
 
 export function seedAccounts() {
   accounts.forEach((creationData) => {
-    createAccount(creationData);
+    createSeedAccounts(creationData);
   });
 }
